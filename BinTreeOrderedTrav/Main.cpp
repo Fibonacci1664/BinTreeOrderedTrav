40db801d7f547d85c48e7c3680cb6fa4f24178ce
Fibonacci <dave.green1664@yahoo.co.uk>
2021-09-05 11:40:24 +0100

    Ok, so with this problem I was able to see the power of recursion. Very handy indeed for tree traversal. As with most things, right tool for the right job, everything has its proper uses.

--- BinTreeOrderedTrav/Main.cpp
+++ BinTreeOrderedTrav/Main.cpp
@@ -1,4 +1,31 @@
 #include<iostream>
+#include <vector>
+
+ //Definition for a binary tree node.
+ struct TreeNode
+ {
+     int val;
+     TreeNode* left;
+     TreeNode* right;
+     TreeNode* parent;
+     TreeNode() : val(0), left(nullptr), right(nullptr), parent(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr), parent(nullptr) {}
+     TreeNode(int x, TreeNode* left, TreeNode* right, TreeNode* parent) : val(x), left(left), right(right), parent(parent) {}
+ };
+ 
+ std::vector<int> treeOrder;
+
+std::vector<int> inorderTraversal(TreeNode* node)
+{
+    if (node != nullptr)
+    {
+        inorderTraversal(node->left);
+        treeOrder.push_back(node->val);
+        inorderTraversal(node->right);
+    }
+
+    return treeOrder;
+}
 
 int main()
 {
